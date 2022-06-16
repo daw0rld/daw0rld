f7e1c7ca4efaadaa7715254efad73caff750240e
daw0rld <100469420+daw0rld@users.noreply.github.com>
2022-06-16 16:42:36 +0200

    Create ratioPlusBasedPlusLossOfMasculineParentalFigure.java

---
+++ ratioPlusBasedPlusLossOfMasculineParentalFigure.java
@@ -0,0 +1,7 @@
+public class ratioPlusBasedPlusLossOfMasculineParentalFigure {
+  public static void main(String[] args){
+    System.out.println("There was an error while loading this file. ERROR CODE: 0x9583718a");
+    // Will this work? 6/2022
+    // It did. 6/2022
+  }
+                          }
